06f7e83b595e4d6a2c256392afbee2cbbde1eebc
CamilaAylenIbarrola <camiaylen0910@gmail.com>
2023-11-30 18:21:57 -0300

    avance del tp

--- Redes/src/Cliente.java
+++ Redes/src/Cliente.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import static java.lang.Integer.parseInt;
 
 
 public class Cliente {
@@ -11,7 +13,6 @@ public class Cliente {
     public static void setPuerto(int puerto) {
         Cliente.puerto = puerto;
     }
-    private DatagramSocket socket; //socket upd
 
     public static String nombreAenviar(){
         Scanner s1=new Scanner(System.in); //para leer el nombre que se va a pasar
@@ -24,35 +25,78 @@ public class Cliente {
         return nombreP;
     }
     public static boolean verificarNombre(String nombre, Scanner lector){ //verifica si los nombres estan en el archivo
-        while(lector.hasNextLine()){
-            String linea=lector.nextLine();
-            linea=linea.split(":")[0]; //separa por los puntos convirtiendolo en array y despues iguala "linea" a la posicion 0 (osea el nombre)
-            if(linea.equals(nombre)){
+        String linea=lector.nextLine();
+        for(int i=0;i<linea.split("-").length;i++){ //entre mas - haya, mas veces se va a correr, y por cada vuelta vamos a interesarnos en la persona que estemos parados
+            String persona=linea.split("-")[i]; //separa por los - convirtiendolo en array, separando a las personas
+            String nombrePerso=persona.split(":")[0];
+            if(nombrePerso.equals(nombre)){
                 return true;
             }
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        String rutaConfig = "D:/Laboratorio/LABOA2023/Redes/src/archivoConfig.txt";
+    public static void mandarMensaje(String emisor, String mensaje, String receptor, DatagramSocket socket, Scanner lectorArchConfig){
+        String mensajeCompleto=receptor + ":" + mensaje + ":" + emisor;
+        InetAddress ipSiguiente;
+        int puertoSiguiente;
+        String linea=lectorArchConfig.nextLine();
+        for(int i=0;i<linea.split("-").length;i++){ //se va a recorrer la lista con todas las personas y por cada vuelta se para en cada persona
+            String persona=linea.split("-")[i]; //crea un array separando a cada persona una de otra por los -
+            String nombrePerso=persona.split(":")[0];
+            if(nombrePerso.equals(emisor)){
+                try{
+                if(i<linea.split("-").length){ //se fija si no es el ultimo en la lista
+                    ipSiguiente=InetAddress.getByName(linea.split("-")[i+1].split(":")[1]); //igualas la ipSiguiente a la ip de la siguiente persona en la que estas parado
+                    puertoSiguiente=parseInt(linea.split("-")[i+1].split(":")[2]); //igualas el puerto al puerto de la siguiente persona en la que estes parado
+                }else{
+                    ipSiguiente=InetAddress.getByName(linea.split("-")[0].split(":")[1]);
+                    puertoSiguiente=parseInt(linea.split("-")[0].split(":")[2]);
+                }} catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                }
+                byte[] b1=mensajeCompleto.getBytes(StandardCharsets.UTF_8); //se crea un buffer
+                DatagramPacket paqueteEnvio=new DatagramPacket(b1, b1.length, ipSiguiente, puertoSiguiente);
+                try {
+                    socket.send(paqueteEnvio);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String rutaConfig = "D:/Redes/Redes/Redes/src/archivoConfig.txt";
         File archivoConfig = new File(rutaConfig);
-        String rutaPersona = "D:/Laboratorio/LABOA2023/Redes/src/archivoPersona.txt";
+        String rutaPersona = "D:/Redes/Redes/Redes/src/archivoPersona.txt";
         File archivoPersona = new File(rutaPersona);
-        try {
+        try{
+            Scanner leerMensaje=new Scanner(System.in);
+            DatagramSocket socket=new DatagramSocket(puerto);
             Scanner scannerLector=new Scanner(archivoConfig);//para leer el archivoConfig.txt
             Scanner scannerLectorP=new Scanner(archivoPersona);//para leer el archivoPersona.txt
             String nombrePersona=Cliente.nombrePropio(scannerLectorP);
-            if(Cliente.verificarNombre(nombrePersona, scannerLector)){
+            if(Cliente.verificarNombre(nombrePersona, scannerLector)) {
                 System.out.println("se ha verificado tu nombre y estas en el archivo");
-                String nombre=Cliente.nombreAenviar();
-                if(Cliente.verificarNombre(nombre, scannerLector)) {
-                    System.out.println("la persona esta en el archivo");
+                while(true){
+                    Thread escucha=new Thread(new ThreadRecibir(socket, scannerLector, nombrePersona));
+                    escucha.start(); //se corre el hilo
+                    String nombreReceptor=Cliente.nombreAenviar();
+                    if(Cliente.verificarNombre(nombreReceptor, scannerLector)){
+                        System.out.println("la persona esta en el archivo");
+                        System.out.println("escriba su mensaje: ");
+                        String mensaje= leerMensaje.next();
+                        Cliente.mandarMensaje(nombrePersona, mensaje, nombreReceptor, socket, scannerLector);
+                    }
                 }
             }else{
                 System.out.println("no estas en el archivo, no podes enviar mensajes");
             }
+
         } catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        } catch (SocketException e) {
             throw new RuntimeException(e);
         }
 
--- Redes/src/ThreadRecibir.java
+++ Redes/src/ThreadRecibir.java
@@ -7,18 +7,29 @@ public class ThreadRecibir implements Runnable{
     // atributos = cosas pasadas x parametro
     private DatagramSocket socket;
     private Scanner lectorArchConfig;
-    private String nombrePActual; //nombre propio
+    private String nombrePActual;
+
+    public ThreadRecibir(DatagramSocket socket, Scanner lectorArchConfig, String nombrePActual) {
+        this.socket = socket;
+        this.lectorArchConfig = lectorArchConfig;
+        this.nombrePActual = nombrePActual;
+    }
 
     @Override
     public void run(){
         byte[]buffer=new byte[256]; //buffer = array de bytes, 256 de espacio en buffer
         DatagramPacket paqueteRecibo=new DatagramPacket(buffer, buffer.length); //se crea el paquete con el buffer dentro que tendra el mensaje a enviar
-        try {
+        try{
             socket.receive(paqueteRecibo);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         String respuesta=new String(paqueteRecibo.getData()); //los datos del datagrama los convierte en string
-        System.out.println("llego el mensaje");
+        System.out.println("llego un mensaje");
+        if(respuesta.split(":")[0] == nombrePActual){
+            System.out.println("te llego el mensaje: " + respuesta.split(":")[1] + "de " + respuesta.split(":")[2]);
+        }else{
+
+        }
     }
 }
